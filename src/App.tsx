dfc58c5fa28d89fad000d2c46af136fcf8c048b6
holvicks <olamilekansegun71@gmail.com>
2025-11-11 13:04:49 +0100

    product description app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <p className='text-black bg-black text-5xl'> Hello </p>
+      </div>
+    </>
+  )
+}
+
+export default App
